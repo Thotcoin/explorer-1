e58d3ccdb369900e20a22e86108cef977522a2f8
unclear0122 <39553384+unclear0122@users.noreply.github.com>
2018-08-18 15:30:04 +0200

    Update and rename batch.js to bunch.js

--- models/batch.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
- 
-var BatchSchema = new Schema({
-  batch_id: { type: String, unique: true, index: true},
-  addresses: { type: Array, default: [] },
-}, {id: false});
-
-module.exports = mongoose.model('Batch', BatchSchema);
